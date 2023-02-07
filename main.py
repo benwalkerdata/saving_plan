3ccac0286688cf255eeb2e69f32e62fd59adfdfd
benwalkerit <benrwalker@icloud.com>
2023-02-07 15:36:42 +0000

    initial code

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+import pandas
+
+envelopes = []
+for i in range(1, 101):
+    envelopes.append(i)
+
+def pick_envelope():
+    
+    print(f"This week's envelope is {random.randint}")
+    
